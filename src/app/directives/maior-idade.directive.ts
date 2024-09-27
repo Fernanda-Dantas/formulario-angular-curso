65fe8fc7df10bf5956bcb19ac543b89b2646b902
Fernanda-Dantas <fer.dantas07@gmail.com>
2024-09-27 10:53:19 -0300

    criei a diretiva de maioridade

--- src/app/directives/maior-idade.directive.ts
+++ src/app/directives/maior-idade.directive.ts
@@ -1,11 +1,29 @@
 import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
-  selector: '[appMaiorIdade]',
-  standalone: true
+  selector: '[maiorIdadeValidator]',
+  standalone: true,
+  providers: [{
+    provide: NG_VALIDATORS,
+    useExisting: MaiorIdadeDirective,
+    multi: true
+  }]
 })
-export class MaiorIdadeDirective {
+export class MaiorIdadeDirective implements Validator {
 
   constructor() { }
+  validate(control: AbstractControl): ValidationErrors | null {
+    const dataNascimento = control.value;
+    const anoNascimento = new Date(dataNascimento).getFullYear();
+    const anoNascimentoMais18 = anoNascimento + 18;
+
+    const anoAtual = new Date().getFullYear();
+
+    const ehMaior = anoNascimentoMais18 <= anoAtual;
+
+    return ehMaior ? null : {'maiorIdadeValidator' : true}
+  }
+
 
 }
