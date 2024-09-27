cc9bb3ba2f5e07b2e92e9fc9bb94f4e1b71b8c9c
Fernanda-Dantas <fer.dantas07@gmail.com>
2024-09-27 12:51:49 -0300

    terminando de validar o campo cep

--- src/app/directives/maior-idade.directive.ts
+++ src/app/directives/maior-idade.directive.ts
@@ -1,5 +1,6 @@
 import { Directive } from '@angular/core';
 import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+import { ConsultaCepService } from '../service/consulta-cep.service';
 
 @Directive({
   selector: '[maiorIdadeValidator]',
@@ -11,16 +12,14 @@ import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@an
 })
 export class MaiorIdadeDirective implements Validator {
 
-  constructor() { }
+  constructor(private ConsultaCepService: ConsultaCepService) { }
+
   validate(control: AbstractControl): ValidationErrors | null {
     const dataNascimento = control.value;
     const anoNascimento = new Date(dataNascimento).getFullYear();
-    const anoNascimentoMais18 = anoNascimento + 18;
-
+    const ateDezoito = anoNascimento + 18;
     const anoAtual = new Date().getFullYear();
-
-    const ehMaior = anoNascimentoMais18 <= anoAtual;
-
+    const ehMaior = ateDezoito <= anoAtual;
     return ehMaior ? null : {'maiorIdadeValidator': true};
   }
 
---
+++ src/app/directives/valida-cep.directive.ts
@@ -0,0 +1,28 @@
+import { ConsultaCepService } from './../service/consulta-cep.service';
+import { Directive } from '@angular/core';
+import { AbstractControl, AsyncValidator, NG_ASYNC_VALIDATORS, ValidationErrors } from '@angular/forms';
+import { map, Observable } from 'rxjs';
+
+@Directive({
+  selector: '[validadorCEP]',
+  providers: [{
+    provide: NG_ASYNC_VALIDATORS,
+    useExisting: ValidaCepDirective,
+    multi: true
+  }]
+})
+export class ValidaCepDirective implements AsyncValidator {
+
+
+  constructor(private ConsultaCepService: ConsultaCepService) { }
+
+  validate(control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    const cep = control.value;
+
+    return this.ConsultaCepService.getConsultaCEP(cep).pipe(map(
+      (resultado: any) => resultado.erro ? {'validadorCEP' : true} : null
+    ))
+      
+  }
+
+}
